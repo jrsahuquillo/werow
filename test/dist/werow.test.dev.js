c22ad0086086fa67b9dcab45c23b5c14a0d4a6cb
jrsahuquillo <jrsahuquillo@gmail.com>
2020-05-30 09:17:03 +0200

    Tests 'Hello World!' api response

--- test/dist/werow.test.dev.js
+++ test/dist/werow.test.dev.js
@@ -1,7 +1,31 @@
 "use strict";
 
-describe('jest', function () {
-  it('works!', function () {
-    expect(true).toBe(true);
+var request = require("supertest");
+
+var app = require("../app");
+
+describe("GET /api ", function () {
+  test("It should respond with a Hello World! object", function _callee() {
+    var response;
+    return regeneratorRuntime.async(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return regeneratorRuntime.awrap(request(app).get("/api"));
+
+          case 2:
+            response = _context.sent;
+            expect(response.body).toEqual({
+              'hello': 'World!'
+            });
+            expect(response.statusCode).toBe(200);
+
+          case 5:
+          case "end":
+            return _context.stop();
+        }
+      }
+    });
   });
 });
--- test/werow.test.js
+++ test/werow.test.js
@@ -1,5 +1,10 @@
-describe('jest' , () => {
-  it('works!', () => {
-    expect(true).toBe(true)
-  })
-})
+const request = require("supertest");
+const app = require("../app");
+
+describe("GET /api ", () => {
+  test("It should respond with a Hello World! object", async () => {
+    const response = await request(app).get("/api");
+    expect(response.body).toEqual({ 'hello': 'World!'});
+    expect(response.statusCode).toBe(200);
+  });
+});
