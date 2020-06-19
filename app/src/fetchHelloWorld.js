3f7c7146c731d257f5cefa3851ec8896bfff644b
jrsahuquillo <jrsahuquillo@gmail.com>
2020-06-19 08:26:58 +0200

    Updates netlify file

--- app/src/fetchHelloWorld.js
+++ app/src/fetchHelloWorld.js
@@ -1,6 +1,6 @@
 export const fetchHelloWorld = async () => {
   const apiUrl = process.env.REACT_APP_API_URL
-  const res = await fetch(apiUrl)
+  const res = await fetch(apiUrl);
   const json = await res.json();
   console.log(json);
   return json;
