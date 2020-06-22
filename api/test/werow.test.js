2582b47fcbfa5f23e2ad37d697ae3776fa082978
jrsahuquillo <jrsahuquillo@gmail.com>
2020-06-22 17:21:16 +0200

    Fixes API tests

--- api/test/werow.test.js
+++ api/test/werow.test.js
@@ -4,7 +4,7 @@ const api = require("../api");
 describe("GET /api ", () => {
   test("It should respond with a Hello World! object", async () => {
     const response = await request(api).get("/api");
-    expect(response.body).toEqual({ 'hello': 'World!'});
+    expect(response.body).toEqual({ 'hello': 'World!!!'});
     expect(response.statusCode).toBe(200);
   });
 });
