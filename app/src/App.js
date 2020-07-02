dce3a0c03350524c86548e4978e93efe8e17b86d
jrsahuquillo <jrsahuquillo@gmail.com>
2020-07-02 18:32:45 +0200

    Fixes create boat

--- app/src/App.js
+++ app/src/App.js
@@ -1,49 +1,11 @@
 import React, {Component} from 'react';
-import { fetchHelloWorld }  from './fetchHelloWorld';
-// import "./App.css"
-import logo from './logo.svg';
+import Boats from "./boats"
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoaded: false,
-      item: []
-    }
-  }
-
-  async handleClick() {
-    const helloWorld = await fetchHelloWorld()
-    this.setState({
-      isLoaded: true,
-      item: helloWorld
-    })
-    setTimeout(() => {
-      this.setState({
-        isLoaded: false
-      })
-    }, 1000);
-  }
-
-  renderMessage() {
-    const { isLoaded, item } = this.state;
-    if(!isLoaded) {
-      return(
-        <p>Click to say something to the World...</p>
-      )
-    }
-    return(
-      <h1>hello: {item.hello}</h1>
-    )
-  }
 
   render () {
     return(
-      <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div><button onClick={ async () => { await this.handleClick() } }>Click me!</button></div>
-        <div>{ this.renderMessage() }</div>
-      </div>
+      <Boats />
     )
   }
 }
